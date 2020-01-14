b35b1a57d68e3b14ca1d85382e4a3a7c57bba86b
rajanimohan22 <mohan.rao.dolla@digital.hmrc.gov.uk>
2020-01-14 10:24:53 +0000

    Initial commit with changes in Mongo dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,8 +9,8 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-play-26" % bootstrapPlayVersion,
-    "uk.gov.hmrc" %% "simple-reactivemongo" % "7.20.0-play-26",
-    "org.reactivemongo" %% "play2-reactivemongo" % "0.17.1-play26",
+    "uk.gov.hmrc" %% "simple-reactivemongo" % "7.22.0-play-26",
+    "org.reactivemongo" %% "play2-reactivemongo" % "0.18.6-play26",
     "uk.gov.hmrc" %% "auth-client" % "2.32.0-play-26",
     ws
   )
@@ -28,7 +28,7 @@ object AppDependencies {
         "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2",
         "org.pegdown" % "pegdown" % "1.6.0",
         "uk.gov.hmrc" %% "reactivemongo-test" % "4.15.0-play-26",
-        "org.reactivemongo" %% "reactivemongo-iteratees" % "0.17.1",
+        "org.reactivemongo" %% "reactivemongo-iteratees" % "0.18.6",
         "com.typesafe.akka" %% "akka-testkit" % "2.5.23",
         "org.mockito" % "mockito-all" % "1.9.5",
         "uk.gov.hmrc" %% "tax-year" % "0.6.0",
