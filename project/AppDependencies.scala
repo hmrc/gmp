a30ab4f4937780f859648d70504a7ce7867744cb
Jack Willett <jack.willett1@digital.hmrc.gov.uk>
2022-12-21 14:23:50 +0000

    Addressed platops-pr-bot comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,22 +4,22 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % "7.12.0",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"        % "0.74.0",
+    "uk.gov.hmrc"            %% "bootstrap-backend-play-28" % "7.12.0",
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-play-28"        % "0.74.0",
+    "org.scalatestplus.play" %% "scalatestplus-play"        % "5.1.0",
+    "org.scalatest"          %% "scalatest"                 % "3.2.9",
+    "com.vladsch.flexmark"    % "flexmark-all"              % "0.35.10",
     ws
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.scalatest"          %% "scalatest"               % "3.2.9",
     "org.scalamock"          %% "scalamock"               % "5.2.0",
-    "org.scalatestplus.play" %% "scalatestplus-play"      % "5.1.0",
     "org.scalatestplus"      %% "scalatestplus-mockito"   % "1.0.0-M2",
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28" % "0.73.0",
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28" % "0.74.0",
     "com.typesafe.akka"      %% "akka-testkit"            % "2.6.20",
     "org.mockito"             % "mockito-all"             % "1.10.19",
     "uk.gov.hmrc"            %% "tax-year"                % "3.0.0",
-    "com.github.tomakehurst"  % "wiremock-standalone"     % "2.27.2",
-    "com.vladsch.flexmark"    % "flexmark-all"            % "0.35.10"
+    "com.github.tomakehurst"  % "wiremock-standalone"     % "2.27.2"
   )
 
   val all: Seq[ModuleID] = compile ++ test
