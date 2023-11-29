35a81de3c2e093084e1d89f6ce0ad62b59c2d976
Jack Willett <jack.willett1@digital.hmrc.gov.uk>
2023-11-29 09:41:46 +0000

    New IF connector created

---
+++ app/connectors/IfConnector.scala
@@ -0,0 +1,262 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors
+
+import java.net.URLEncoder
+import java.util.concurrent.TimeUnit
+import com.google.inject.{Inject, Singleton}
+import metrics.ApplicationMetrics
+import models._
+import play.api.http.Status._
+import play.api.{Configuration, Logging}
+import uk.gov.hmrc.http._
+import uk.gov.hmrc.play.audit.AuditExtensions._
+import uk.gov.hmrc.play.audit.http.connector.AuditConnector
+import uk.gov.hmrc.play.audit.model.{DataEvent, EventTypes}
+import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
+import uk.gov.hmrc.http.HttpClient
+
+import scala.concurrent.{ExecutionContext, Future}
+import uk.gov.hmrc.http.HttpReads.Implicits.readRaw
+
+trait IfGetResponse
+
+sealed trait IfPostResponse
+
+case object IfGetSuccessResponse extends IfGetResponse
+
+case object IfGetHiddenRecordResponse extends IfGetResponse
+
+case object IfGetNotFoundResponse extends IfGetResponse
+
+case class IfGetErrorResponse(e: Exception) extends IfGetResponse
+
+case object IfGetUnexpectedResponse extends IfGetResponse
+
+case object IfGetUnprocessableEntityResponse extends IfGetResponse
+
+@Singleton
+class IfConnector @Inject()(val runModeConfiguration: Configuration,
+                             metrics: ApplicationMetrics,
+                             http: HttpClient,
+                             auditConnector: AuditConnector,
+                             val servicesConfig: ServicesConfig)(implicit ec: ExecutionContext) extends Logging {
+
+  private val PrefixStart = 0
+  private val PrefixEnd = 1
+  private val NumberStart = 1
+  private val NumberEnd = 8
+  private val SuffixStart = 8
+  private val SuffixEnd = 9
+
+  val serviceKey: String = servicesConfig.getConfString("ifs.key", "")
+  val serviceEnvironment: String = servicesConfig.getConfString("ifs.environment", "")
+
+  val baseURI = "pensions/individuals/gmp"
+  val baseSconURI = "pensions/gmp/scon"
+  val calcURI = s"$serviceURL/$baseURI"
+  val validateSconURI = s"$serviceURL/$baseSconURI"
+  lazy val serviceURL: String = servicesConfig.baseUrl("ifs")
+
+  val citizenDetailsUrl: String = servicesConfig.baseUrl("citizen-details")
+
+  def validateScon(userId: String, scon: String)(implicit hc: HeaderCarrier): Future[ValidateSconResponse] = {
+
+    val uri =
+      s"""$validateSconURI/${
+        scon.substring(PrefixStart,
+          PrefixEnd).toUpperCase
+      }/${
+        scon.substring(NumberStart,
+          NumberEnd)
+      }/${
+        scon.substring(SuffixStart,
+          SuffixEnd).toUpperCase
+      }/validate"""
+
+    doAudit("gmpSconValidation", userId, scon, None, None, None)
+    logger.debug(s"[IfConnector][validateScon] Contacting IF at $uri")
+
+    val startTime = System.currentTimeMillis()
+
+    val result = http.GET[HttpResponse](uri, headers = ifHeaders)(implicitly[HttpReads[HttpResponse]], hc.copy(authorization = None), ec).map { response =>
+
+      metrics.ifConnectorTimer(System.currentTimeMillis() - startTime, TimeUnit.MILLISECONDS)
+      metrics.ifConnectorStatus(response.status)
+
+      response.status match {
+        case OK | UNPROCESSABLE_ENTITY => response.json.as[ValidateSconResponse]
+        case errorStatus: Int => {
+          logger.error(s"[IfConnector][validateScon] : IF returned code $errorStatus and response body: ${response.body}")
+          throw UpstreamErrorResponse("IF connector validateScon failed", errorStatus, INTERNAL_SERVER_ERROR)
+        }
+      }
+
+    }
+
+    result
+  }
+
+  def calculate(userId: String, request: CalculationRequest)(implicit hc: HeaderCarrier): Future[CalculationResponse] = {
+
+    val paramMap: Map[String, Option[Any]] = Map(
+      "revalrate" -> request.revaluationRate, "revaldate" -> request.revaluationDate, "calctype" -> request.calctype,
+      "request_earnings" -> request.requestEarnings, "dualcalc" -> request.dualCalc, "term_date" -> request.terminationDate)
+
+    val surname = URLEncoder.encode((if (request.surname.length < 3) {
+      request.surname
+    } else {
+      request.surname.substring(0, 3)
+    }).toUpperCase, "UTF-8")
+
+    val firstname = URLEncoder.encode(request.firstForename.charAt(0).toUpper.toString, "UTF-8")
+
+    val uri =
+      s"""$calcURI/scon/${
+        request.scon.substring(PrefixStart,
+          PrefixEnd).toUpperCase
+      }/${
+        request.scon.substring(NumberStart,
+          NumberEnd)
+      }/${
+        request.scon.substring(SuffixStart,
+          SuffixEnd).toUpperCase
+      }/nino/${request.nino.toUpperCase}/surname/$surname/firstname/$firstname/calculation/${
+        buildEncodedQueryString(paramMap) match {
+          case "?" => ""
+          case params => params
+        }
+      }"""
+
+    doAudit("gmpCalculation", userId, request.scon, Some(request.nino), Some(request.surname), Some(request.firstForename))
+    logger.debug(s"[IfConnector][calculate] Contacting IF at $uri")
+
+    val startTime = System.currentTimeMillis()
+
+    val result = http.GET[HttpResponse](uri, headers = ifHeaders)(implicitly[HttpReads[HttpResponse]], hc.copy(authorization = None), ec).map { response =>
+
+      metrics.ifConnectorTimer(System.currentTimeMillis() - startTime, TimeUnit.MILLISECONDS)
+      metrics.ifConnectorStatus(response.status)
+
+      response.status match {
+        case OK => response.json.as[CalculationResponse]
+        case UNPROCESSABLE_ENTITY => {
+          logger.info("[IfConnector][calculate] : IF returned code 422")
+          CalculationResponse(request.nino,
+            422,
+            None,
+            None,
+            None,
+            Scon(request.scon.substring(PrefixStart, PrefixEnd).toUpperCase, request.scon.substring(NumberStart, NumberEnd).toInt, request.scon.substring(SuffixStart, SuffixEnd).toUpperCase),
+            Nil)
+        }
+        case BAD_REQUEST => {
+          logger.info("[IfConnector][calculate] : IF returned code 400")
+          CalculationResponse(request.nino,
+            400,
+            None,
+            None,
+            None,
+            Scon(request.scon.substring(PrefixStart, PrefixEnd).toUpperCase, request.scon.substring(NumberStart, NumberEnd).toInt, request.scon.substring(SuffixStart, SuffixEnd).toUpperCase),
+            Nil)
+        }
+        case errorStatus: Int => {
+          logger.error(s"[IfConnector][calculate] : IF returned code $errorStatus and response body: ${response.body}")
+          throw UpstreamErrorResponse("IF connector calculate failed", errorStatus, INTERNAL_SERVER_ERROR)
+        }
+      }
+    }
+
+    result
+
+  }
+
+  private def ifHeaders =
+    Seq("Authorization" -> s"Bearer $serviceKey",
+      "Gov-Uk-Originator-Id" -> servicesConfig.getConfString("ifs.originator-id", ""),
+      "Environment" -> serviceEnvironment)
+
+  private def buildEncodedQueryString(params: Map[String, Any]): String = {
+    val encoded = for {
+      (name, value) <- params if value != None
+      encodedValue = value match {
+        case Some(x) => URLEncoder.encode(x.toString, "UTF8")
+      }
+    } yield name + "=" + encodedValue
+
+    encoded.mkString("?", "&", "")
+  }
+
+  private def doAudit(auditTag: String,
+                      userID: String,
+                      scon: String,
+                      nino: Option[String],
+                      surname: Option[String],
+                      firstForename: Option[String])(implicit hc: HeaderCarrier): Unit = {
+
+    val auditDetails: Map[String, String] = Map("userId" -> userID,
+      "scon" -> scon,
+      "nino" -> nino.getOrElse(""),
+      "firstName" -> firstForename.getOrElse(""),
+      "surname" -> surname.getOrElse(""))
+
+    val auditResult = auditConnector.sendEvent(
+      DataEvent("gmp",
+        EventTypes.Succeeded,
+        tags = hc.toAuditTags(auditTag, "N/A"),
+        detail = hc.toAuditDetails() ++ auditDetails))
+
+    auditResult.failed.foreach({
+      case e: Throwable => logger.warn("[IfConnector][doAudit] : auditResult: " + e.getMessage, e)
+    })
+  }
+
+  def getPersonDetails(nino: String)(implicit hc: HeaderCarrier): Future[IfGetResponse] = {
+
+    val npsHeaders = Seq("Authorization" -> s"Bearer $serviceKey",
+      "Gov-Uk-Originator-Id" -> servicesConfig.getConfString("ifs.originator-id", ""),
+      "Environment" -> serviceEnvironment)
+
+    val startTime = System.currentTimeMillis()
+    val url = s"$citizenDetailsUrl/citizen-details/$nino/etag"
+
+    logger.debug(s"[IfConnector][getPersonDetails] Retrieving person details from $url")
+
+    http.GET[HttpResponse](url, headers = ifHeaders)(implicitly[HttpReads[HttpResponse]], hc.copy(authorization = None), ec) map { response =>
+
+      metrics.mciConnectionTimer(System.currentTimeMillis() - startTime, TimeUnit.MILLISECONDS)
+
+      response.status match {
+        case LOCKED =>
+          metrics.mciLockCount()
+          IfGetHiddenRecordResponse
+        case NOT_FOUND => IfGetNotFoundResponse
+        case OK => IfGetSuccessResponse
+        case INTERNAL_SERVER_ERROR => IfGetUnexpectedResponse
+        case _ => IfGetUnexpectedResponse
+      }
+
+    } recover {
+      case e: UpstreamErrorResponse if e.statusCode == NOT_FOUND => IfGetNotFoundResponse
+      case e: Exception =>
+        logger.error("[IfConnector][getPersonDetails] Exception thrown getting individual record from IF", e)
+        metrics.mciErrorCount()
+        IfGetErrorResponse(e)
+    }
+  }
+
+}
