fc638eaa2b3cd831cd06300ade9a58e5ff639a57
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2024-02-15 15:02:20 +0000

    DLS-9921: Corrects location of dependencies in compile/test

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,22 +10,21 @@ object AppDependencies {
   val compile = Seq(
     "uk.gov.hmrc"            %% "bootstrap-backend-play-28" % "7.12.0",
     "uk.gov.hmrc"            %% "tax-year"                % "3.0.0",
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-play-28"        % "0.73.0",
     ws
   )
 
   val test: Seq[ModuleID] = Seq(
     "com.typesafe.akka"      %% "akka-testkit"            % "2.6.20",
-    "org.scalatestplus.play" %% "scalatestplus-play"        % "5.1.0"    % "test",
-    "org.scalatest"          %% "scalatest"                 % "3.2.9"    % "test",
-    "com.vladsch.flexmark"    % "flexmark-all"              % "0.35.10"  % "test",
+    "org.scalatestplus.play" %% "scalatestplus-play"        % "5.1.0",
+    "org.scalatest"          %% "scalatest"                 % "3.2.9",
+    "com.vladsch.flexmark"    % "flexmark-all"              % "0.35.10",
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28" % "0.74.0",
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-play-28"        % "0.73.0"   % "test",
     "org.scalamock"          %% "scalamock"               % "5.2.0",
-    "org.scalatestplus"      %% "scalatestplus-mockito"   % "1.0.0-M2" % "test",
+    "org.scalatestplus"      %% "scalatestplus-mockito"   % "1.0.0-M2",
     "org.mockito"             % "mockito-all"             % "1.10.19",
     "com.github.tomakehurst"  % "wiremock-standalone"     % "2.27.2"
-  )
-//  ).map(_ % "test")
+  ).map(_ % "test")
 
   val all: Seq[ModuleID] = compile ++ test
 
