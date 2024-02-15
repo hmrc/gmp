79c3703f9a54e65b51f9f210fc07d4b9ca78166c
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2024-02-15 14:57:48 +0000

    DLS-9921: WIP reorganising AppDependencies in order to correctly place dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,22 +9,23 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"            %% "bootstrap-backend-play-28" % "7.12.0",
-    "org.scalatestplus.play" %% "scalatestplus-play"        % "5.1.0"    % "test",
-    "org.scalatest"          %% "scalatest"                 % "3.2.9"    % "test",
-    "com.vladsch.flexmark"    % "flexmark-all"              % "0.35.10"  % "test",
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-play-28"        % "0.73.0"   % "test",
+    "uk.gov.hmrc"            %% "tax-year"                % "3.0.0",
     ws
   )
 
   val test: Seq[ModuleID] = Seq(
+    "com.typesafe.akka"      %% "akka-testkit"            % "2.6.20",
+    "org.scalatestplus.play" %% "scalatestplus-play"        % "5.1.0"    % "test",
+    "org.scalatest"          %% "scalatest"                 % "3.2.9"    % "test",
+    "com.vladsch.flexmark"    % "flexmark-all"              % "0.35.10"  % "test",
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28" % "0.74.0",
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-play-28"        % "0.73.0"   % "test",
     "org.scalamock"          %% "scalamock"               % "5.2.0",
     "org.scalatestplus"      %% "scalatestplus-mockito"   % "1.0.0-M2" % "test",
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28" % "0.74.0",
-    "com.typesafe.akka"      %% "akka-testkit"            % "2.6.20",
     "org.mockito"             % "mockito-all"             % "1.10.19",
-    "uk.gov.hmrc"            %% "tax-year"                % "3.0.0",
     "com.github.tomakehurst"  % "wiremock-standalone"     % "2.27.2"
   )
+//  ).map(_ % "test")
 
   val all: Seq[ModuleID] = compile ++ test
 
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -5,7 +5,11 @@ resolvers += Resolver.url("HMRC-open-artefacts-ivy", url("https://open.artefacts
 resolvers += Resolver.jcenterRepo
 
 addSbtPlugin("uk.gov.hmrc"       %  "sbt-auto-build"        % "3.20.0")
-addSbtPlugin("org.playframework" %  "sbt-plugin"            % "3.0.1")
+// TODO: REVERT PLAY 3.0 UPGRADE ONCE APP DEPENDENCIES MOVED TO CORRECT COMPILE/TEST
+//addSbtPlugin("org.playframework" %  "sbt-plugin"            % "3.0.1")
+addSbtPlugin("com.typesafe.play" %  "sbt-plugin"            % "2.8.19")
 addSbtPlugin("uk.gov.hmrc"       %  "sbt-distributables"    % "2.5.0")
 addSbtPlugin("org.scalastyle"    %% "scalastyle-sbt-plugin" % "1.0.0" exclude("org.scala-lang.modules", "scala-xml_2.12"))
 addSbtPlugin("org.scoverage"     %  "sbt-scoverage"         % "2.0.9")
+
+ThisBuild / libraryDependencySchemes += "org.scala-lang.modules" %% "scala-xml" % VersionScheme.Always
