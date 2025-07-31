40ac7b4e6936ef33c4206561a827445bf8f61de1
GUJJALAPUDIA <amarnath.gujjalapudi@digital.hmrc.gov.uk>
2025-07-31 12:30:51 +0100

    Update HipCalculationRequest.scala

--- app/models/HipCalculationRequest.scala
+++ app/models/HipCalculationRequest.scala
@@ -62,8 +62,8 @@ case class HipCalculationRequest(schemeContractedOutNumber: String,
                                  secondForename: Option[String],
                                  revaluationRate: Option[EnumRevaluationRate.Value],
                                  calculationRequestType: Option[EnumCalcRequestType.Value],
-                                 revaluationDate: String,
-                                 terminationDate: String,
+                                 revaluationDate: Option[String],
+                                 terminationDate: Option[String],
                                  includeContributionAndEarnings: Boolean,
                                  includeDualCalculation: Boolean)
 
@@ -98,8 +98,8 @@ object HipCalculationRequest {
       secondForename = None,
       revaluationRate = revalEnum,
       calculationRequestType = calcTypeEnum,
-      revaluationDate = calcReq.revaluationDate.getOrElse("(NONE)"),
-      terminationDate = calcReq.terminationDate.getOrElse("(NONE)"),
+      revaluationDate = calcReq.revaluationDate,
+      terminationDate = calcReq.terminationDate,
       includeContributionAndEarnings = calcReq.requestEarnings.contains(1),
       includeDualCalculation = calcReq.dualCalc.contains(1)
     )
